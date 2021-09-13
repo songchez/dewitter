aa49e59ccdfd09049bd2f9cc27296089f7c8726c
songchez <tama4840@gmail.com>
2021-09-13 19:13:46 +0900

    master

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,9 @@
 function Home ({user_id}) {
-    return (<div><h1>홈로리홈홈</h1></div>)
+    return (
+      <div>
+        <h1>홈로리홈홈</h1>
+        <p>{user_id}</p>
+      </div>
+    );
 }
 export default Home;
