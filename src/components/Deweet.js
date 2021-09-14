569ee784bb461ba0b11f05f1fc15f5e663370211
songchez <tama4840@gmail.com>
2021-09-14 19:47:11 +0900

    cooool

---
+++ src/components/Deweet.js
@@ -0,0 +1,23 @@
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+function AppRouters({user}){
+    return (
+      <Router>
+        <Switch>
+          {user ? ( // User is signed In
+            <Route exact path="/">
+              <Home user = {user} />
+            </Route> // User is signed out
+          ) : (
+            <Route exact path="/">
+              <Auth />
+            </Route>
+          )}
+        </Switch>
+      </Router>
+    );
+};
+
+export default AppRouters;
