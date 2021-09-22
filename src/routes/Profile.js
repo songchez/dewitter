cf1925bf03587025540dcb080bcc04e802d490f2
songchez <tama4840@gmail.com>
2021-09-22 17:35:01 +0900

    voodoochild

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,31 @@
-const Profile =({user}) => {
+import { collection, getDocs, orderBy, query, where } from "@firebase/firestore";
+import { db } from "m_base";
+import { useEffect } from "react";
+
+const Profile = ({ user }) => {
+  const getMyDeweets = async () => {
+    const deweets = query(
+      collection(db, "msg"),
+      where("createdId", "==", user.uid),
+      orderBy("createdAt"),
+    );
+    console.log("쿼리",deweets);
+    const querySnapshot = await getDocs(deweets);
+    console.log("스냅샷",querySnapshot); 
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+    });
+  };
+
+  useEffect(() => {
+    getMyDeweets();
+  },);
+
   return (
-      <div>
-        <h3>{user.uid}</h3>
-        <h1>Edit your Profile</h1>
-      </div>
+    <div>
+      <h3>{user.uid}</h3>
+      <h1>Edit your Profile</h1>
+    </div>
   );
 };
 export default Profile;
