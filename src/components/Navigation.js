66ff34a73769ef0dede42857aff14f9b219312e6
songchez <tama4840@gmail.com>
2021-09-24 18:23:30 +0900

    배포_1.0.0

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,20 +4,31 @@ import { Link } from "react-router-dom";
 
 const Navigation = ({ user }) => {
   return (
-    <nav>
-      <ul style={{ display: "flex", justifyContent: "center", marginTop: 50 }}>
-        <li>
-          <Link to="/" style={{ marginRight: 30 }}>
-            <FontAwesomeIcon icon={faHome} color={"#04AAFF"} size="2x" />
-          </Link>
-        </li>
-        <li>
-          <Link to="/profile">
-              <img src={user.photoURL} alt="userprofilePhoto" className="navProfile"/>
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <ul
+          style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
+        >
+                <div className="top__Logo">
+        <h4>DEWITTER</h4>
+      </div>
+          <li>
+            <Link to="/">
+              <div className="navHome"><FontAwesomeIcon icon={faHome} color={"#04AAFF"} size="2x" /></div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/profile">
+              <img
+                src={user.photoURL}
+                alt="userprofilePhoto"
+                className="navProfile"
+              />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
